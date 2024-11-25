d63fd4417845e0a7d0ee930bbbd6d3dd622788f9
Ceci Martinez <122909681+cmartinez1997@users.noreply.github.com>
2024-11-25 14:59:40 -0700

    wrnagling script to siubset final dtagframe

---
+++ scripts/wrangling_dataset.R
@@ -0,0 +1,17 @@
+## Cecilia Martinez
+## November 25, 2024
+## This script is to wrangle the dataset that we will be using in our analysis 
+## for our final project in STATS574E
+
+
+
+# load packages -----------------------------------------------------------
+
+library(readr)
+library(dplyr)
+
+
+
+# read in the data --------------------------------------------------------
+
+data <- read_csv("data/final_proj.csv")
